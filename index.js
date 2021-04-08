415b5667fcf9e06dbe41f37306154eaeefaa4ec7
CCooper92 <am10ac17@gmail.com>
2021-04-08 10:41:12 -0700

    3rd

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const express = require("express")
 const cors = require("cors")
 const app = express() //doesn't have to be server
 const port = process.env.PORT || 9000
+const path = require("path")
+
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname,"client/build")))
 
 app.use("/api/",(_,res)=> {
     res.json({data:"API is accounted for"}) //API you want to use will go here so people can use
